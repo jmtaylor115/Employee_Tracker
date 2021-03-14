4606fd532a1ca307c5f0ce52f5df83fad40e6435
jmtaylor115 <jmtaylor115@yahoo.com>
2021-03-14 07:18:45 -0400

    Created methods for finding managers and roles

--- js/index.js
+++ js/index.js
@@ -12,10 +12,90 @@ async function main() {
     });
 };
 
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id' + connection.threadID);
+    afterConnection();
+});
+
+afterConnection = () => {
+    connection.query('SELECT * FROM Department', function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+    connection.query('SELECT * FROM Role', function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+    connection.query('SELECT * FROM Employee', function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+};
+
 //(at start) inquire options to view departments, roles, employees
 //add a department, role, employee
 //update a role
+async function getManagers() {
+    var tableRows = await connection.query("SELECT * FROM Employee WHERE manager_id IS NULL");
+    var managers = [];
 
+    for(var Employee of tableRows) {
+        managers.push(Employee.first_name + " " + Employee.last_name);
+    }
+    return managers;
+
+}
+
+async function getRoles() {
+    var tableRows = await connection.query("SELECT title FROM Role");
+    var roles = [];
+
+    for(var row of tableRows) {
+        roles.push(row.title);
+    }
+    return roles;
+    
+}
+
+async function getDepartments() {
+    var tableRows = await connection.query("SELECT name FROM Department ");
+    var departmentNames = [];
+
+    for(var row of tableRows) {
+        departmentNames.push(row.name);
+    }
+    return departmentNames;
+    
+}
+
+async function getDepartmentID() {
+    var tableRows = await connection.query("SELECT * FROM Department WHERE Department.name=?");
+    
+
+    
+}
+
+async function getManagers() {
+    var query = "SELECT * FROM Employee WHERE manager_id IS NULL";
+
+    
+}
+
+async function getManagers() {
+    var query = "SELECT * FROM Employee WHERE manager_id IS NULL";
+
+    
+}
+
+async function getManagers() {
+    var query = "SELECT * FROM Employee WHERE manager_id IS NULL";
+
+    
+}
 //(view departments) display formatted table showing department names and ids
 
 //(view roles) display job title, role id, department that role is under, role salary
